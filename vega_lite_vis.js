9e15de13e0cd4d937975fd8cdd432e21f22e9533
jcpia1 <145896106+jcpia1@users.noreply.github.com>
2023-10-16 04:06:11 +1100

    Made some sweet progress

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "map.vg.json";
 vegaEmbed("#map", vg_1, {actions:false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "donut_chart.vg.json";
+vegaEmbed("#donut_chart", vg_2, {actions:false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
