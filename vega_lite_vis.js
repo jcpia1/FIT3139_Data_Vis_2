f78f3d47bfd718b5ba6854ad81c6c1160b909e4c
jcpia1 <145896106+jcpia1@users.noreply.github.com>
2023-10-15 20:48:39 +1100

    .

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
 var vg_1 = "map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {actions:false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
