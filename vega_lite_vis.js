7f59994dbcaba0bd37c6d6c7ef4f1f8abba8c9dc
jcpia1 <145896106+jcpia1@users.noreply.github.com>
2023-10-16 21:03:47 +1100

    Map is working now
    Just need to get the STUPID HTML AND CSS working....

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,33 @@
-var vg_1 = "map.vg.json";
-vegaEmbed("#map", vg_1, {actions:false}).catch(console.error);
+// First, load the map spec dynamically
+fetch('map.vg.json')
+    .then(response => response.json())
+    .then(data => {
+        // Store the loaded spec in the mapSpec variable
+        var mapSpec = data;
+
+        function updateMapVisualization(startYear, endYear) {
+            var modifiedMapSpec = JSON.parse(JSON.stringify(mapSpec)); // Deep copy of the spec
+
+            // Find the filter transform and update its condition
+            for (var i = 0; i < modifiedMapSpec.transform.length; i++) {
+                var transform = modifiedMapSpec.transform[i];
+                if (transform.filter && transform.filter.includes("startYear") && transform.filter.includes("endYear")) {
+                    transform.filter = `(year(datum.ignition_date) >= ${startYear} && year(datum.ignition_date) <= ${endYear})`;
+                    break;
+                }
+            }
+
+            // Re-render the map visualization with the modified spec
+            vegaEmbed('#map', modifiedMapSpec, {actions:false}).catch(console.error);
+        }
+
+        // Initial render of the map
+        vegaEmbed('#map', mapSpec, {actions:false}).catch(console.error);
+
+        // Expose the updateMapVisualization function to the global scope so it can be called from the HTML slider
+        window.updateMapVisualization = updateMapVisualization;
+    })
+    .catch(console.error);
 
 // Load the donut_chart spec dynamically
 fetch('donut_chart.vg.json')
@@ -8,7 +36,7 @@ fetch('donut_chart.vg.json')
         // Store the loaded spec in the donut_chart variable
         var donut_chart = data;
 
-        function updateVisualization(startYear, endYear) {
+        function updateDCVisualization(startYear, endYear) {
             var modifiedSpec = JSON.parse(JSON.stringify(donut_chart)); // Deep copy of the spec
 
             // Find the filter transform and update its condition
@@ -28,6 +56,6 @@ fetch('donut_chart.vg.json')
         vegaEmbed('#donut_chart', donut_chart, {actions:false}).catch(console.error);
 
         // Expose the updateVisualization function to the global scope so it can be called from the HTML slider
-        window.updateVisualization = updateVisualization;
+        window.updateVisualization = updateDCVisualization;
     })
     .catch(console.error);
