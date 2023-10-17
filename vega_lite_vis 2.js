ab55e0d6f928db9952e46b431758c134df4997d4
Jerome <jerome.piahana@gmail.com>
2023-10-17 17:47:43 +1100

    Working on two implementations. Single visualisation, and separate

---
+++ vega_lite_vis 2.js
@@ -0,0 +1,4 @@
+vegaEmbed('#map', 'vis2.vg.json', {actions:false}).then(function(result) {
+    // Access the Vega view instance as result.view
+  }).catch(console.error);
+  
--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,61 +1,63 @@
-// First, load the map spec dynamically
-fetch('map.vg.json')
-    .then(response => response.json())
-    .then(data => {
-        // Store the loaded spec in the mapSpec variable
-        var mapSpec = data;
+// Variables to hold the Vega views
+let mapView;
+let donutView;
 
-        function updateMapVisualization(startYear, endYear) {
-            var modifiedMapSpec = JSON.parse(JSON.stringify(mapSpec)); // Deep copy of the spec
+// Fetch the map spec
+const fetchMap = fetch('map.vg.json').then(response => response.json());
 
-            // Find the filter transform and update its condition
-            for (var i = 0; i < modifiedMapSpec.transform.length; i++) {
-                var transform = modifiedMapSpec.transform[i];
-                if (transform.filter && transform.filter.includes("startYear") && transform.filter.includes("endYear")) {
-                    transform.filter = `(year(datum.ignition_date) >= ${startYear} && year(datum.ignition_date) <= ${endYear})`;
-                    break;
-                }
+// Fetch the donut_chart spec
+const fetchDonutChart = fetch('donut_chart.vg.json').then(response => response.json());
+
+// Once both fetches are complete
+Promise.all([fetchMap, fetchDonutChart]).then(([mapSpec, donut_chart]) => {
+    
+    // Embed the map visualization and keep a reference to its view
+    vegaEmbed('#map', mapSpec, {actions:false})
+        .then(result => {
+            mapView = result.view;
+        })
+        .catch(console.error);
+
+    // Embed the donut chart visualization and keep a reference to its view
+    vegaEmbed('#donut_chart', donut_chart, {actions:false})
+        .then(result => {
+            donutView = result.view;
+        })
+        .catch(console.error);
+
+    // Initialize the noUiSlider
+    var slider = document.getElementById('yearRangeSlider');
+    noUiSlider.create(slider, {
+        start: [1899, 2020],
+        range: {
+            'min': 1899,
+            'max': 2023
+        },
+        format: {
+            to: function(value) {
+                return parseInt(value);
+            },
+            from: function(value) {
+                return parseInt(value);
             }
+        },
+        tooltips: [true, true],
+        connect: true
+    });
 
-            // Re-render the map visualization with the modified spec
-            vegaEmbed('#map', modifiedMapSpec, {actions:false}).catch(console.error);
+    slider.noUiSlider.on('update', function(values, handle) {
+        var startYear = parseInt(values[0]);
+        var endYear = parseInt(values[1]);
+        document.getElementById('yearRange').innerText = `${startYear} - ${endYear}`;
+
+        // Update the map visualization using the Vega view API
+        if (mapView) {
+            mapView.signal('startYear', startYear).signal('endYear', endYear).run();
         }
 
-        // Initial render of the map
-        vegaEmbed('#map', mapSpec, {actions:false}).catch(console.error);
-
-        // Expose the updateMapVisualization function to the global scope so it can be called from the HTML slider
-        window.updateMapVisualization = updateMapVisualization;
-    })
-    .catch(console.error);
-
-// Load the donut_chart spec dynamically
-fetch('donut_chart.vg.json')
-    .then(response => response.json())
-    .then(data => {
-        // Store the loaded spec in the donut_chart variable
-        var donut_chart = data;
-
-        function updateDCVisualization(startYear, endYear) {
-            var modifiedSpec = JSON.parse(JSON.stringify(donut_chart)); // Deep copy of the spec
-
-            // Find the filter transform and update its condition
-            for (var i = 0; i < modifiedSpec.transform.length; i++) {
-                var transform = modifiedSpec.transform[i];
-                if (transform.filter && transform.filter.includes("startYear") && transform.filter.includes("endYear")) {
-                    transform.filter = `(year(datum.ignition_date) >= ${startYear} && year(datum.ignition_date) <= ${endYear})`;
-                    break;
-                }
-            }
-
-            // Re-render the visualization with the modified spec
-            vegaEmbed('#donut_chart', modifiedSpec, {actions:false}).catch(console.error);
+        // Update the donut chart visualization using the Vega view API
+        if (donutView) {
+            donutView.signal('startYear', startYear).signal('endYear', endYear).run();
         }
-
-        // Initial render of the donut chart
-        vegaEmbed('#donut_chart', donut_chart, {actions:false}).catch(console.error);
-
-        // Expose the updateVisualization function to the global scope so it can be called from the HTML slider
-        window.updateVisualization = updateDCVisualization;
-    })
-    .catch(console.error);
+    });
+}).catch(console.error);
